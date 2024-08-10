88e619321c7959e9c3e348b3dda6762cae9b6711
pylover <vahid.mardani@gmail.com>
2024-08-10 22:52:31 +0330

    Feature: ensure the installation script is running as root and create the gitea configuration and working directory

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@ function gitea_ensurechecksum () {
   echo "Checking Gitea binary file's checksum..."
   if [ ! -f ${GITEA_BINFILE_CHKSUMFILE} ]; then
     gpg -q --keyserver ${GITEA_GPGKEYSERVER} --recv ${GITEA_GPGKEYID}
-    wget --directory-prefix ${DLDIR} ${GITEA_DOWNLOADURL}.asc
+    wget --directory-prefix ${DLDIR} ${GITEA_DLURL}.asc
   fi
   gpg -q --verify ${GITEA_BINFILE_CHKSUMFILE} ${GITEA_BINFILE_LOCAL} \
     && (echo "Checksum OK: ${GITEA_BINFILE_LOCAL}" && return 0)  \
@@ -18,7 +18,7 @@ function gitea_download () {
   if [ ! -f ${GITEA_BINFILE_LOCAL} ]; then
     mkdir -p ${DLDIR}
     echo "Downloading Gitea binary..."
-    wget --directory-prefix ${DLDIR} ${GITEA_DOWNLOADURL}
+    wget --directory-prefix ${DLDIR} ${GITEA_DLURL}
     gitea_ensurechecksum || exit 1
   fi
 }
--- install.sh
+++ install.sh
@@ -13,15 +13,47 @@ if [ "${OSTYPE}" != "linux-gnu" ] || [ ! -f "/etc/debian_version" ]; then
 fi
 
 
+# Check the privileges
+if [ "$(whoami)" != "root" ]; then
+  err "Please run this script as root"
+fi
+
+
 # Dependencies
 read -p "Do you want to install required debian packages? [N/y] " 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  sudo apt-get install wget git
+  apt-get install wget git
 fi
 
 
 # Downloading the specific binary if required
 gitea_download
+
+
+# Create the git user
+adduser \
+   --system \
+   --shell /bin/bash \
+   --gecos 'Git Version Control' \
+   --group \
+   --disabled-password \
+   --home /home/git \
+   git
+
+
+# Create required directory structure
+# TODO: /etc/gitea is temporarily set with write permissions for user git so 
+# that the web installer can write the configuration file. After the 
+# installation is finished, it is recommended to set permissions to read-only 
+# using:
+#   chmod 750 /etc/gitea
+#   chmod 640 /etc/gitea/app.ini
+mkdir -p ${GITEA_WORKINGDIR}/{custom,data,log}
+chown -R git:git ${GITEA_WORKINGDIR}
+chmod -R 750 ${GITEA_WORKINGDIR}
+mkdir ${GITEA_CONFIGDIR}
+chown root:git ${GITEA_CONFIGDIR}
+chmod 770 ${GITEA_CONFIGDIR}
 
 
 echo $GITEA_BINFILE_NAME
